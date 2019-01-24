70c9a06aee469792f61ecb0a9120b7f1e8360961
Piotr Bernatowicz <piotrbernatowicz@tutanota.com>
2019-01-24 10:00:44 +0100

    Add sections team, portfolio, contact

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function() {
+	let galleryTemplate = document.getElementById('gallery-template').innerHTML;
+	let galleryList = '';
+	for(var i= 0; i < galleryData.length; i++) {
+		galleryList += Mustache.render(galleryTemplate, galleryData[i]);
+	}
+
+	let carouselMain = document.getElementById('carousel-main');
+	carouselMain.innerHTML = galleryList;
+
+	let carousel = document.querySelector('.carousel');
+	let flkty = new Flickity(carousel, {
+		hash: true, wrapAround: true
+	});
+})();
